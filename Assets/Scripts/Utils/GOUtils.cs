fe92ba7915d4cb20cdbfe2cd548742eb28aaf4c1
JoVanko <49305713+zhaohuanke123@users.noreply.github.com>
2024-01-13 21:07:46 +0800

    goutils

---
+++ Assets/Scripts/Utils/GOUtils.cs
@@ -0,0 +1,18 @@
+[LuaCallCSharp]
+public static class GOUtils
+{
+    public static void SetPosition(GameObject go, float x, float y, float z)
+    {
+        go.transform.position = new Vector3(x, y, z);
+    }
+
+    public static void SetRotation(GameObject go, float x, float y, float z)
+    {
+        go.transform.rotation = Quaternion.Euler(x, y, z);
+    }
+
+    public static void SetLocalPosition(GameObject go, float x, float y, float z)
+    {
+        go.transform.localPosition = new Vector3(x, y, z);
+    }
+}
