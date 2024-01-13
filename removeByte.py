7aa4f7e13513bd8de181272e4fd80fb82394dece
JoVanko <49305713+zhaohuanke123@users.noreply.github.com>
2024-01-13 22:03:20 +0800

    1

--- removeByte.py
+++ removeByte.py
@@ -9,7 +9,8 @@ def removeByte(path):
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(".bytes"):
-                os.rename(os.path.join(root, file), os.path.join(root, file[:-6]))
+                os.rename(os.path.join(root, file),
+                          os.path.join(root, file[:-6]))
 
 
 if __name__ == "__main__":
